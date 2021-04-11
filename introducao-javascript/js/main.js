05c47be55c1b4c31d3c4b94439c0a486ae4af01d
nely de faria <nelly.nf54@gmail.com>
2021-04-11 19:25:00 -0300

    Conhecendo dicionário

--- introducao-javascript/js/main.js
+++ introducao-javascript/js/main.js
@@ -1,10 +1,14 @@
-var lista = ['maça', 'pêra', 'laranja'];
-lista.push('uva'); // adiciona elemento
-lista.pop(); // removendo último elemento
+
+var fruta = {nome:'maça', cor:'vermelha'};
+console.log(fruta);
+
+//var lista = ['maça', 'pêra', 'laranja'];
+//lista.push('uva'); // adiciona elemento
+//lista.pop(); // removendo último elemento
 //console.log(lista.length); // tamanho do array
 //console.log(lista.reverse()); // imprimindo lista ao contrário
-console.log(lista.toString());
-console.log(lista.join(' - '));
+//console.log(lista.toString());
+//console.log(lista.join(' - '));
 /* var nome = 'Rafael Galleani';
 var idade = 29;
 var frase = 'Japão é o melhor time do mundo';
