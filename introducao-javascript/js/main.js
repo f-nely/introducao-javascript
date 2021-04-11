e299caf3278191ff3f33b8b95b7d14b2fa84b518
nely de faria <nelly.nf54@gmail.com>
2021-04-11 18:59:22 -0300

    Criando variável frase e conhecendo a função replace

--- introducao-javascript/js/main.js
+++ introducao-javascript/js/main.js
@@ -1,5 +1,9 @@
 var nome = 'Rafael Galleani';
 var idade = 29;
+var frase = 'Japão é o melhor time do mundo';
 
 //alert(nome + ' tem ' + idade + ' anos');
-alert(`${nome} tem ${idade} anos`);
+//alert(`${nome} tem ${idade} anos`);
+console.log(nome);
+console.log(idade);
+console.log(frase.replace('Japão', 'Brasil'));
